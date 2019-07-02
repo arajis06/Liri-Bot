05a7eb211a72c18a66b51ed2d7271d2dc8561ad3
ALEXIS RAJIS <arajis06@gmail.com>
2019-07-02 01:42:45 -0400

    added all functions

--- liri.js
+++ liri.js
@@ -7,7 +7,6 @@ var fs = require('fs');
 //Packages
 var axios = require('axios');
 var moment = require('moment');
-moment().format();
 
 var keys = require('./keys.js');
 var Spotify = require('node-spotify-api');//using Spotify Api & getting the key from key.js
@@ -30,7 +29,7 @@ switch (command) {
       break;
     
     case "spotify-this-song":
-      spotifySong(value);
+      spotifyThisSong(value);
       break;
     
     case "movie-this":
@@ -42,22 +41,104 @@ switch (command) {
       break;
     };
 
-    function concertThis(artist) {
-        // We then run the request with axios module on a URL with a JSON
-        axios.get("http:// rest.bandsintown.com&apikey=trilogy").then(
-        function(response) {
-            // Then we print out the imdbRating
-            console.log("The movie's rating is: " + response.data.imdbRating);
+//FUNCTION Bands In Town================================================================================
+function concertThis(artist) {
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+    // Running the request with Axios module on a URL with a JSON
+    axios.get(queryUrl)
+    .then(function(response) { 
+        for (var i = 0; i < response.data.length; i++) {
+
+            var eventDate = moment(response.data[0].datetime).format('MM/DD/YYYY');
+            var concertResults =
+            "-----------------**Concert Info**-----------------" +
+            "\nVenue Name: " + response.data[i].venue.name +
+            "\nVenue Location: " + response.data[i].venue.city +
+            "\nEvent Date: " + eventDate +
+            "\n--------------------------------------------------";
+            console.log(concertResults);
         }
-);
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+}
+//==========================================================================================================
 
-    }
-    
+//FUNCTION Spotify Song=====================================================================================
+function spotifyThisSong(song) {
+    if (!song) {
+        song = "The Sign, Ace of Base";
+    } 
+    spotify
+    .search({ 
+        type: 'track', 
+        query: song, 
+        limit: 5 
+    })
+    .then(function(response) {
+        for (var i = 0; i < 5; i++) {
+            
+            var spotifyResults = 
+            "----------------**Song Info**-----------------" +
+            "\nArtist: " + response.tracks.items[i].album.artists[0].name +
+            "\nTrack: " + response.tracks.items[i].name +
+            "\nAlbum: " + response.tracks.items[i].album.name +
+            "\nPreview URL: " + response.tracks.items[i].preview_url +
+            "\n--------------------------------------------";
+            console.log(spotifyResults);
+        }
+    })
+    .catch(function(error) {
+        console.log(error);
+    });    
+}
+//==========================================================================================================
 
+//FUNCTION Movie This=====================================================================================
+function movieThis(movie) {
+    if(!movie) {
+        movie = 'Mr. Nobody';
+        console.log("-------------**Check Out This Movie**------------")
+        console.log("If you haven't watched " + movie + ", then you should: http://www.imdb.com/title/tt0485947/" + 
+        "\nIt's on NetFlix!" + "\n----------------------------------------------");
+     };
 
+     var queryUrl = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
 
+     // Running the request with Axios module on a URL with a JSON
+     axios.get(queryUrl)
+     .then(function(response) { 
+ 
+             var movieResults =
+             "-----------------**Movie Info**-----------------" +
+             "\nTitle: " + response.data.Title +
+             "\nYear Released: " + response.data.Year +
+             "\nImdb Rating: " + response.data.imdbRating +
+             "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value +
+             "\nCountry Produced: " + response.data.Country +
+             "\nLanguage: " + response.data.Language +
+             "\nPlot: " + response.data.Plot +
+             "\nActors/Actresses: " + response.data.Actors +
+             "\n-------------------------------------------------";
+             console.log(movieResults);
+     })
+    .catch(function(error) {
+        console.log(error);
+    });    
+}
+//==========================================================================================================
 
+//FUNCTION Do This=====================================================================================
+function doThis() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+            return console.log(error);
+        }
 
-
-
-
+        var dataArray = data.split(",");
+        spotifyThisSong(dataArray[0], dataArray[1]);
+    });
+}
+//==========================================================================================================
